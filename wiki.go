2addb6fe265364273b872765ad884936f8e1e1a3
Jakob Symchych <99896548+jakobsym@users.noreply.github.com>
2022-08-21 07:21:12 -0500

    Update wiki.go

--- wiki.go
+++ wiki.go
@@ -82,15 +82,6 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	}
 }
 
-/*func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-	m := validPath.FindStringSubmatch(r.URL.Path)
-	if m == nil {
-		http.NotFound(w, r)
-		return "", errors.New("invalid Page Title")
-	}
-	return m[2], nil // title is located at m[2]
-} */
-
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
